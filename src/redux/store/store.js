4d236ef065ed7a29b5bae58c8a0e6e69574d211c
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-06-09 16:43:04 +0700

    Bloodoonation management staff

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import blogPostsReducer from "../features/blogPostsSlice";
 import bloodReducer from "../features/bloodSlice";
+import bloodDonation from "../features/bloodDonationSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     blogPosts: blogPostsReducer,
     blood: bloodReducer,
+    donationRequests: bloodDonation
   },
 });
