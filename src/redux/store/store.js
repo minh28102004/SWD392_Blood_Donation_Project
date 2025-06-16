a6acc9bf55394bb81869f3df9458a674dc0184bf
izumi6803 <tuananh6803@gmail.com>
2025-06-16 19:23:33 +0700

    crud

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,21 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import blogPostsReducer from "../features/blogPostsSlice";
 import bloodReducer from "../features/bloodSlice";
-<<<<<<< HEAD
+
 import bloodInventoryReducer from "../features/bloodInvSlice";
-=======
+
 import bloodDonation from "../features/bloodDonationSlice";
->>>>>>> 4d236ef065ed7a29b5bae58c8a0e6e69574d211c
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     blogPosts: blogPostsReducer,
     blood: bloodReducer,
-<<<<<<< HEAD
+
     bloodInventory: bloodInventoryReducer,
-=======
+
     donationRequests: bloodDonation
->>>>>>> 4d236ef065ed7a29b5bae58c8a0e6e69574d211c
-  },
-});
+  }})
